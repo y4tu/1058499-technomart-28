37dd81087deab2cf8e92a5a79b98880985a0dd43
y4tu <65165172+y4tu@users.noreply.github.com>
2020-06-29 13:47:22 +0300

    Небольшие правки и форматирование кода

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 var mapOpen = document.querySelector(".minimap-link");
 var map = document.querySelector(".popup-map");
-var feedbackOpen = document.querySelector(".feedback-link");
-var feedback = document.querySelector(".popup-feedback");
-var feedbackName = feedback.querySelector("#name-field");
-var feedbackEmail = feedback.querySelector("#email-field");
-var feedbackLetter = feedback.querySelector("#letter-field");
-var feedbackForm = feedback.querySelector(".popup-form");
 var cartOpen = document.querySelector(".goods-buy-link");
 var cartOpenList = document.querySelectorAll(".goods-buy-link");
 var cart = document.querySelector(".popup-cart");
 var close = document.querySelector(".cross");
 var closeList = document.querySelectorAll(".cross");
+var closeCart = document.querySelector(".popup-continue")
 var isStorageSupport = true;
 var storage = "";
 
 try {
-  storage = localStorage.getItem("feedbackName");
+    storage = localStorage.getItem("feedbackName");
 }
 
 catch (err) {
-isStorageSupport = false;
+    isStorageSupport = false;
 }
 
 for (let i = 0; i < cartOpenList.length; i++) {
@@ -41,39 +36,51 @@ for (let i = 0; i < closeList.length; i++) {
     })
 }
 
+closeCart.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cart.classList.remove("show");
+})
+
 window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (cart.classList.contains("show")) {
-      evt.preventDefault();
-      cart.classList.remove("show");
+    if (evt.keyCode === 27) {
+      if (cart.classList.contains("show")) {
+        evt.preventDefault();
+        cart.classList.remove("show");
+      }
     }
-  }
 });
 
 mapOpen.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  map.classList.add("show");
+    evt.preventDefault();
+    map.classList.add("show");
 });
 
 window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (map.classList.contains("show")) {
-      evt.preventDefault();
-      map.classList.remove("show");
+    if (evt.keyCode === 27) {
+      if (map.classList.contains("show")) {
+        evt.preventDefault();
+        map.classList.remove("show");
+      }
     }
-  }
 });
 
+var feedbackOpen = document.querySelector(".feedback-link");
+var feedback = document.querySelector(".popup-feedback");
+var feedbackName = feedback.querySelector("#name-field");
+var feedbackEmail = feedback.querySelector("#email-field");
+var feedbackLetter = feedback.querySelector("#letter-field");
+var feedbackForm = feedback.querySelector(".popup-form");
+
 feedbackOpen.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  feedback.classList.add("show");
-  if (storage) {
-      feedbackName.value = storage;
-      feedbackEmail.focus();
-  }
-  else {
-      feedbackName.focus();
-  }
+    evt.preventDefault();
+    feedback.classList.add("show");
+    if (storage) {
+        feedbackName.value = storage;
+        feedbackEmail.focus();
+      }
+      else {
+        feedbackName.focus();
+      }
 });
 
 feedbackForm.addEventListener("submit", function (evt) {
@@ -82,18 +89,19 @@ feedbackForm.addEventListener("submit", function (evt) {
       feedback.classList.remove("error");
       feedback.offsetWidth = feedback.offsetWidth;
       feedback.classList.add("error");
-  } else {
+    }
+    else {
       localStorage.setItem("feedbackName", feedbackName.value);
       localStorage.setItem("feedbackEmail", feedbackEmail.value);
-  }
+    }
 });
 
 window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (feedback.classList.contains("show")) {
-      evt.preventDefault();
-      feedback.classList.remove("show");
-      feedback.classList.remove("error");
+    if (evt.keyCode === 27) {
+      if (feedback.classList.contains("show")) {
+        evt.preventDefault();
+        feedback.classList.remove("show");
+        feedback.classList.remove("error");
+      }
     }
-  }
 });
